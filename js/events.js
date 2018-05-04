bad40424b1d7c9e109382f0c187fe85af153b683
yuxichen11@gmail.com <yuxichen11@gmail.com>
2018-05-04 01:38:19 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,22 @@
 //define functions here
-
+function getIt() {
+ $('p').on('click',function() {
+   alert('Hey!');
+ });
+}
+function frameIt() {
+  $('img').on('load',function() {
+    $('img').addClass('tasty');
+  });
+}
+function pressIt() {
+  $('#typing').on('keydown',function(event) {
+    if (event.which === 71) {
+      alert('Thats the G spot');
+    }
+  });
+}
 $(document).ready(function(){
-
 // call functions here
 
 });
